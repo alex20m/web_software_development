a18e8e55f9425d7589c04b7b7238185c675aaf8d
alexm <alex.mecklin@aalto.fi>
2024-05-29 22:43:18 +0300

    Exercise 6.3

--- project/app.js
+++ project/app.js
@@ -1,7 +1,17 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import * as feedback from "./feedbacks.js";
 
 const app = new Hono();
+feedback.setFeedback("1", 0);
+feedback.setFeedback("2", 0);
+feedback.setFeedback("3", 0);
 
-app.get("/", (c) => c.text("Hi there!"));
+app.get("/feedbacks/:id", async (c) => {
+    const count = await feedback.getFeedback(c.req.param("id"));
+    return await c.text(`Feedback: ${count}`);
+})
 
+app.post("/feedbacks/:id", async (c) => {
+    await feedback.incrementCount(c.req.param("id"));
+})
 export default app;
---
+++ project/feedbacks.js
@@ -0,0 +1,22 @@
+
+const setFeedback = async (Id, count) => {
+  const kv = await Deno.openKv();
+  await kv.set([`${Id}`], count);
+};
+
+const getFeedback = async (Id) => {
+  const kv = await Deno.openKv();
+  const feedback = await kv.get([Id]);
+  return feedback.value; 
+};
+
+const incrementCount = async (Id) => {
+  let count = await getFeedback(`${Id}`);
+  console.log(count)
+  count = parseFloat(count);
+  count++;
+  console.log(count)
+  await setFeedback(`${Id}`, count);
+};
+
+export { setFeedback, getFeedback, incrementCount };
