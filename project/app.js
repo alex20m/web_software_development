d40b137e23668d32e332b109aeec6042d6f1ada4
alexm <alex.mecklin@aalto.fi>
2024-06-05 21:55:35 +0300

    Exercise 9.4.2

--- project/app.js
+++ project/app.js
@@ -1,27 +1,26 @@
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import * as songService from "./songService.js";
-
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import * as feedback from "./feedbacks.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
-
 const app = new Hono();
 
-let data = {
-    songs: []
-};
+feedback.setFeedback("1", 0);
+feedback.setFeedback("2", 0);
+feedback.setFeedback("3", 0);
 
 app.get("/", async (c) => {
-    const retval = await songService.listSongs();
-    data.songs = retval
-    return await c.html(eta.render("index.eta", data))
-});
+    return await c.html(eta.render("index.eta"));
+})
 
-app.post("/songs", async (c) =>{
-    const songData = await c.req.parseBody();
-    songService.addSong(songData);
-    return  c.redirect("/");
-});
+app.get("/feedbacks/:id", async (c) => {
+    const count = await feedback.getFeedback(c.req.param("id"));
+    return await c.text(`Feedback ${c.req.param("id")}: ${count}`);
+})
+
+app.post("/feedbacks/:id", async (c) => {
+    await feedback.incrementCount(c.req.param("id"));
+    return c.redirect("/");
+})
 
 export default app;
-
