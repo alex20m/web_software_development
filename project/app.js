d7de070065a9c8de18c23dfaefa406f75f8c32fd
alexm <alex.mecklin@aalto.fi>
2024-06-06 18:12:17 +0300

    10.1

--- project/app.js
+++ project/app.js
@@ -1,26 +1,12 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
-import * as feedback from "./feedbacks.js";
+import * as todoController from "./todoController.js";
 
-const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
 
-feedback.setFeedback("1", 0);
-feedback.setFeedback("2", 0);
-feedback.setFeedback("3", 0);
-
-app.get("/", async (c) => {
-    return await c.html(eta.render("index.eta"));
-})
-
-app.get("/feedbacks/:id", async (c) => {
-    const count = await feedback.getFeedback(c.req.param("id"));
-    return await c.text(`Feedback ${c.req.param("id")}: ${count}`);
-})
-
-app.post("/feedbacks/:id", async (c) => {
-    await feedback.incrementCount(c.req.param("id"));
-    return c.redirect("/");
-})
+app.get("/todos", todoController.showForm);
+app.get("/todos/:id", todoController.showTodo);
+app.post("/todos", todoController.createTodo);
+app.post("/todos/:id", todoController.updateTodo);
+app.post("/todos/:id/delete", todoController.deleteTodo);
 
 export default app;
