e78839c4590927ff51a0cc796739f7605bc4af19
alexm <alex.mecklin@hotmail.com>
2024-06-16 20:21:00 +0300

    round 20 done

---
+++ CRUD_database/api/deps.js
@@ -0,0 +1,3 @@
+export { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+export { postgres };
---
+++ CRUD_user_management/deps.js
@@ -0,0 +1,4 @@
+export { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+export { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+export { postgres };
